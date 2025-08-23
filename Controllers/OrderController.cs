33a7a620f0f6d685ecef0dfed6bc43d68efa7ed8
shadrackbila <shadrackbila2000@gmail.com>
2025-08-24 00:59:43 +0200

    added the payment gateway

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -18,5 +18,49 @@ public class OrderController : Controller
         return View(viewModel);
     }
 
+    [HttpPost]
+    public IActionResult PayWithPayFast(decimal amount, string itemName)
+    {
+
+        string site = "https://sandbox.payfast.co.za/eng/process";
+
+
+        var model = new Dictionary<string, string>
+    {
+        { "merchant_id", "10041413" },
+        { "merchant_key", "sc4ufc371j03p" },
+   { "return_url", "https://c5e3728f58a6.ngrok-free.app/payment/success" },
+    { "cancel_url", "https://c5e3728f58a6.ngrok-free.app/payment/cancel" },
+    { "notify_url", "https://c5e3728f58a6.ngrok-free.app/payment/notify" },
+        { "amount", amount.ToString("F2") },
+        { "item_name", itemName }
+    };
+
+        var query = string.Join("&", model.Select(kvp => $"{kvp.Key}={Uri.EscapeDataString(kvp.Value)}"));
+        var redirectUrl = $"{site}?{query}";
+
+        return Redirect(redirectUrl);
+    }
+
+    [Route("payment/success")]
+    public IActionResult Success()
+    {
+        // Payment successful
+        return View();
+    }
+
+    public IActionResult Cancel()
+    {
+        // Payment cancelled
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Notify()
+    {
+        // PayFast sends server-to-server notifications here
+        return Ok();
+    }
+
 
 }
--- obj/Debug/net9.0/TimelyTastes.AssemblyInfo.cs
+++ obj/Debug/net9.0/TimelyTastes.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TimelyTastes")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b2bfea66b76799dc4c0b8e4563d348c402e48cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b4e03474b0b14dda7cf41422c2cde0e7cbcd42d3")]
 [assembly: System.Reflection.AssemblyProductAttribute("TimelyTastes")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TimelyTastes")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
